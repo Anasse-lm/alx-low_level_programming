24c451a65bc6ec6360d129f7991645a8004b7663
Anasse-lm <fuctura.20@gmail.com>
2023-09-07 09:47:24 -0700

    Task 5: print with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 
 /**
  * main - Entry point
- *
+ * Description: This is the main function that
+ *              prints a message to standard output.
  * Return : Always 0 (Success)
  */
 int main(void)
