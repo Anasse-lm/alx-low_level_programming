66bc3a992c5b61ec38f516d8575f9c5a912032a1
Anasse-lm <fuctura.20@gmail.com>
2023-09-09 14:26:26 -0700

    task1 : tolower and putchar functions

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: This the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(tolower(c));
+	}
+	putchar('\n');
+	return (0);
+}
