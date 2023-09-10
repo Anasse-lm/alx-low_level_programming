7edd16b93716f792ce00058fec420da85b87a6ac
Anasse-lm <fuctura.20@gmail.com>
2023-09-09 19:40:33 -0700

    task4: alphabet 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: this is main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
