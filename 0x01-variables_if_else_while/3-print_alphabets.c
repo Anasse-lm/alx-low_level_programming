d633bc1f4cc9c8b9898866d7206bea93f8c48a0a
Anasse-lm <fuctura.20@gmail.com>
2023-09-09 19:18:11 -0700

    task 3: alphabet 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: this is main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cLower,cUpper;
+
+	for (cLower = 'a'; cLower <= 'z'; cLower++)
+	{
+		putchar(cLower);
+	}
+	for (cUpper = 'A'; cUpper<= 'Z'; cUpper++)
+	{
+		putchar(cUpper);
+	}
+	putchar('\n');
+	return (0);
+}
