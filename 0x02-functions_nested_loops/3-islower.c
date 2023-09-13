81e926dbd9691a6cbedade0362e75668b1d27e45
Anasse-lm <fuctura.20@gmail.com>
2023-09-13 13:00:54 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 /**
  * _islower - checks if the alphabet is lower
  *
- * Return: success 1.
+ * Description: this is islower function
+ *
+ * Return: On success 1.
  * On error: returns 0.
  */
 int _islower(int c)
 {
-	if (c > 'a'-1 && c < 'z'+1)
+	if (c > 'a' - 1 && c < 'z' + 1)
 	{
 		return (1);
 	}
