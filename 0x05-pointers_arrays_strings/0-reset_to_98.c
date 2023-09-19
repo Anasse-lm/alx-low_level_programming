ef97f6d0b439f0d7b8987943b23d66345f289aa5
Anasse-lm <fuctura.20@gmail.com>
2023-09-19 11:52:21 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98: updates the value to 98
+ * reset_to_98 - updates the value to 98
  * @n: pointer to int value
  *
  * Description: This function takes a pointer to an integer as a parameter
