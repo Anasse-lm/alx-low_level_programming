dbfd6bf5f954e70d101440397b2626f90ba0626c
Anasse-lm <fuctura.20@gmail.com>
2023-09-19 18:03:40 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy a string into other.
+ * @dest: destination
+ * @src: source
+ * Return: char *.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return strcpy(dest, src);
+}
