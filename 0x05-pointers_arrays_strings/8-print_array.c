3868433c926cc880367c0d45aaf3a14dd59ba64f
Anasse-lm <fuctura.20@gmail.com>
2023-09-19 17:24:55 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints elemets of an array separated with comma.
+ * @a: array
+ * @n: lenght of array
+ * 
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[n-1]);
+}
