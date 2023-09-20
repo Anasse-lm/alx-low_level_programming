2296a2db19d738927f5d2ad2dd4b8649a1def5fd
Anasse-lm <fuctura.20@gmail.com>
2023-09-19 17:14:26 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int i, len = strlen(str);
+
+	for (i = (len - 1) / 2; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
