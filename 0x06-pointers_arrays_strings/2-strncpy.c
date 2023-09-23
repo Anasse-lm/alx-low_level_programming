065403bdddbc84673b9ca93a5824c6e0606369ab
Anasse-lm <fuctura.20@gmail.com>
2023-09-23 02:02:34 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - copying str into other
+ * @dest: destination
+ * @src: source
+ * @n: lenght
+ * Return: char *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
