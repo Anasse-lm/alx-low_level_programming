1a77457b823fbc44f855d8db19d0e99fe882a21d
Anasse-lm <fuctura.20@gmail.com>
2023-09-24 00:18:03 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: number
+ * Return: int.
+ */
+int print_last_digit(int n)
+{
+	if (n > 0)
+	{
+		_putchar((n % 10) + '0');
+		return (n % 10);
+	}
+	else if (n < 0)
+	{
+		_putchar((-n % 10) + '0');
+		return (-n % 10);
+	}
+	else {
+		_putchar((-n % 10) + '0');
+                return (-n % 10);
+	}
+}
