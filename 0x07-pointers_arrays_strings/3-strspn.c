58c8965d9c6cbbdb3103ea61111f223050f8db54
Anasse-lm <fuctura.20@gmail.com>
2023-09-25 08:46:12 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,23 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int num = 0;
-	int alen = strlen(accept), i, j;
+	int alen = strlen(accept), i, j, found = 0;
 
-	for (i = 0; i < alen + 1; i++)
+	for (i = 0; i <= alen; i++)
 	{
 		for (j = 0; j < alen; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				num++;
+				found = 1;
+				break;
 			}
 		}
+		if (!found)
+		{
+			break;
+		}
+		num++;
 	}
 	return (num);
 }
